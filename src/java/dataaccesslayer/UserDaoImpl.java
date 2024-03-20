a6ea44afdeeb726c570f3f2f7424b29e04d7d698
LiyingOttawa <149939808+LiyingOttawa@users.noreply.github.com>
2024-03-20 11:21:37 -0400

    DAO_start

--- src/java/dataaccesslayer/UserDaoImpl.java
+++ src/java/dataaccesslayer/UserDaoImpl.java
@@ -1,82 +1,81 @@
 package dataaccesslayer;
 
 import java.io.Serializable;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import model.UserDto;
 
-public class UserDaoImpl implements DAO<UserDto> {
+public class UserDaoImpl extends DAOImpl<UserDto> {
+
+    final static String SQL_INSERT = "INSERT INTO user (Name, Role_id, Email, Password) VALUES (?, ?, ?, ?)";
+    final static String SQL_DELETE_ALL = "DELETE FROM user";
+    final static String SQL_DELETE = "DELETE FROM user WHERE user_id = ?";
+    final static String SQL_UPDATE = "UPDATE user SET Name = ?, Role_id = ?, Email = ? WHERE user_id = ?";
+    final static String SQL_RETRIEVE = "SELECT * FROM user WHERE user_id = ?";
+    final static String SQL_RETRIEVE_ALL = "SELECT * FROM user";
 
     @Override
- public int insert(UserDto user) {
-    String sql = "INSERT INTO user (Name, Role_id, Email, Password) VALUES (?, ?, ?, ?)";
-    
-    DataSource dataSource = new DataSource();
+    public int insert(UserDto user) {
 
-    try {
-        return dataSource.executeUpdate(sql, user.getUsername(), user.getRoleId(), user.getEmail(), user.getPassword());
-    } catch (Exception ex) {
-        ex.printStackTrace();
-        return 0;
-    }
-}
-
-    @Override
-    public int delete(Serializable id) {
-        String sql = (Integer) id == 0 ? "DELETE FROM user" : "DELETE FROM user WHERE user_id = ?";
-        
         try {
-            return DataSource.executeUpdate(sql, id);
+            return dataSource.executeUpdate(SQL_INSERT, user.getUsername(), user.getRoleId(), user.getEmail(), user.getPassword());
         } catch (Exception ex) {
             ex.printStackTrace();
             return 0;
         }
     }
 
- @Override
-public int update(UserDto user) {
-    String sql = "UPDATE user SET Name = ?, Role_id = ?, Email = ? WHERE user_id = ?";
-
-    try {
-        return DataSource.executeUpdate(sql, user.getUsername(), user.getRoleId(), user.getEmail(), user.getUserId());
-    } catch (Exception ex) {
-        ex.printStackTrace();
-        return 0;
-    }
-}
-
-
-@Override
-public UserDto findById(Serializable id) {
-    String sql = "SELECT * FROM user WHERE user_id = ?";
-    
-    try {
-        ResultSet resultSet = DataSource.executeQuery(sql, id);
-        if (resultSet.next()) {
-            UserDto user = new UserDto();
-            user.setUserId(resultSet.getInt("user_id"));
-            user.setEmail(resultSet.getString("first_name"));
-            user.setRoleId(resultSet.getInt("role_id"));
-            return user;
+    @Override
+    public int delete(Serializable id) {
+        try {
+            if (id == null) {
+                return dataSource.executeUpdate(SQL_DELETE_ALL);
+            } else {
+                return dataSource.executeUpdate(SQL_DELETE, id);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return 0;
         }
-    } catch (Exception ex) {
-        ex.printStackTrace();
     }
-    return null;
-}
+
+    @Override
+    public int update(UserDto user) {
+
+        try {
+            return dataSource.executeUpdate(SQL_UPDATE, user.getUsername(), user.getRoleId(), user.getEmail(), user.getUserId());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public UserDto findById(Serializable id) {
+        try (PreparedStatement statement = dataSource.query(SQL_RETRIEVE, id); ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+                UserDto user = new UserDto();
+                user.setUserId(resultSet.getInt("user_id"));
+                user.setEmail(resultSet.getString("name"));
+                user.setRoleId(resultSet.getInt("role_id"));
+                return user;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
     @Override
     public List<UserDto> findAll() {
         List<UserDto> users = new ArrayList<>();
-        String sql = "SELECT * FROM user";
-        
-        try {
-            ResultSet resultSet = DataSource.executeQuery(sql);
+        try (PreparedStatement statement = dataSource.query(SQL_RETRIEVE_ALL); ResultSet resultSet = statement.executeQuery()) {
             while (resultSet.next()) {
                 UserDto user = new UserDto();
-               user.setUserId(resultSet.getInt("user_id"));
-                user.setEmail(resultSet.getString("first_name"));
+                user.setUserId(resultSet.getInt("user_id"));
+                user.setEmail(resultSet.getString("name"));
                 user.setRoleId(resultSet.getInt("role_id"));
                 users.add(user);
             }
