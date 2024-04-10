dfc96e5e36f0acafbdf8b55128a8a0baecb09a74
LiyingOttawa <149939808+LiyingOttawa@users.noreply.github.com>
2024-04-10 11:25:29 -0400

    working on donation and claim

--- src/java/controller/DonationControllerServlet.java
+++ src/java/controller/DonationControllerServlet.java
@@ -1,8 +1,10 @@
 
 package controller;
 
+import businesslayer.AuthService;
 import businesslayer.ItemListingService;
 import businesslayer.NavigationHelper;
+import businesslayer.TransactionService;
 import dataaccesslayer.DAO;
 import dataaccesslayer.ItemListingDaoImpl;
 import java.io.IOException;
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.ItemListingDTO;
+import viewmodel.DonationViewModelItem;
+import viewmodel.SaleViewModelItem;
 
 /**
  *
@@ -20,7 +24,10 @@ import model.ItemListingDTO;
  */
 @WebServlet(name = "DonationControllerServlet", urlPatterns = {"/donation/*"})
 public class DonationControllerServlet extends HttpServlet {    
-    private final ItemListingService dataService = new ItemListingService();
+    AuthService dataService = new AuthService();
+
+    private final ItemListingService itemListingService = new ItemListingService();
+    private final TransactionService transactionService = new TransactionService();
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -37,15 +44,24 @@ public class DonationControllerServlet extends HttpServlet {
         switch(action)
         {
             case "/claim":
-                NavigationHelper.goTo(request,response,"/views/donation/claim.jsp");
-                break;
-            case "/claims":
-                NavigationHelper.goTo(request,response,"/views/donation/claims.jsp");
+                String idForView = request.getParameter("id");
+                if (idForView == null) {
+                    NavigationHelper.HandleError(response, new Exception("Bad Reqeust with not id."));
+                } else {
+                    DonationViewModelItem item = itemListingService.buidDonationViewModelItem(Integer.parseInt(idForView));
+                    if (item == null) {
+                        NavigationHelper.HandleError(response, new Exception("Bad Reqeust. Item not found"));
+                    } else {
+                        request.setAttribute("item", item);
+                        NavigationHelper.goTo(request,response,"/views/donation/claim.jsp");
+                    }
+
+                }
                 break;
             default:
                 String itemType = request.getParameter("itemType");
                 String expireDays = request.getParameter("expireDays");
-                request.setAttribute("viewModel", dataService.buidDonationViewModel(itemType, expireDays));
+                request.setAttribute("viewModel", itemListingService.buidDonationViewModel(itemType, expireDays));
                 NavigationHelper.goTo(request,response,"/views/donation/list.jsp");
             break;
             
@@ -64,6 +80,36 @@ public class DonationControllerServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String action = request.getPathInfo();
+        try {
+            String id = request.getParameter("id");
+            switch (action) {
+                    default:
+                    case "/claim":
+                        String quantity = request.getParameter("quantity");
+                        boolean successSubmit = false;
+                        if(dataService.isLoggedIn(request) && quantity != null)
+                        {
+                           successSubmit = transactionService.donate(dataService.getUserId(request),Integer.parseInt(id),Integer.parseInt(quantity));
+                        }
+
+                        if (successSubmit) {
+                            response.sendRedirect("/sale/");
+                        } else {
+                            NavigationHelper.HandleError(response, new Exception("Failed. Please retry"));
+                        }
+                        break;
+
+                }
+
+            // Perform add operation
+            // Example: call a service method to add the item
+            // addItem(itemName, unit, locationId, createDate, userId, itemTypeId, quantity, expirDate, price, status, statusDate);
+            // Redirect the user to a successSubmit page or back to the previous page
+            // Example: response.sendRedirect("successSubmit.jsp");
+        } catch (Exception e) {
+            NavigationHelper.HandleError(response, e);
+        }
     }
 
     /**
